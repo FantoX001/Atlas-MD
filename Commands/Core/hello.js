f6891179bbaf9321d93de053fa322d50912bffc7
Yakashi13 <83496819+Yakashi13@users.noreply.github.com>
2023-02-28 12:53:42 +0530

    Update hello.js

--- Commands/Core/hello.js
+++ Commands/Core/hello.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: "hi",
-    alias: ["hello", "hey", "yoo", "moshi"],
+    alias: ["hello", "hey", "yoo", "moshi moshi"],
     desc: "Say hello to bot.",
     react: "💜",
     category: "Core",
     start: async(Miku, m,{pushName,prefix}) => {
-        await Miku.sendMessage(m.from,{text:`\n\n𝐘𝐨𝐨 *${pushName}* 😼!!,\n\nI am *${botName}* 😎`},{quoted:m})
+        await Miku.sendMessage(m.from,{text:`\n𝐘𝐨𝐨 *${pushName}* 😼 *_Senpai_*!!,\n\nI am *${botName}* 😎.\n Provide Me a Command!!`},{quoted:m})
     }
 }
